9f4611a20375b1acf05ef7323d77937776e9aacd
coolKIH <caozihuahh@gmail.com>
2017-02-06 09:21:26 +0800

    push

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
-        <title>Hao's Blog</title>
+        <title>Hao</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
     </head>
     <body>
