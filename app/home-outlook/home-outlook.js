29d1df5cef7b9a274aabe4155b08be95703a3ce1
coolKIH <caozihuahh@gmail.com>
2017-02-14 19:53:00 +0800

    new style

--- app/home-outlook/home-outlook.js
+++ app/home-outlook/home-outlook.js
@@ -5,5 +5,10 @@ angular.module('homeOutlook',[])
   .component('homeOutlook',{
     templateUrl: 'home-outlook/home-outlook.html',
     controller: function() {
+
+      this.setTheme = function(theme) {
+        this.pickedTheme = theme;
+      };
+      this.setTheme('home');
     }
   });
