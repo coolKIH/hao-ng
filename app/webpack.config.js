21dd876a58e97fb29a5d12707709e29071f7ea53
coolKIH <caozihuahh@gmail.com>
2017-02-16 13:49:23 +0800

    minified css

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,13 @@
 /**
  * Created by hao on 17-2-16.
  */
-module.exports = {
-  entry: ["./app.js", "./home-outlook/home-outlook.js", "./core/navigation-bar/navigation-bar.js",
-    "./articles/articles.js", "./intro/intro.js", "./showcase/showcase.js", "./article-detail/article-detail.js"
-  ],
-  output: {
-    filename: "bundle.js"
+module.exports = [
+  {
+    entry: ["./app.js", "./home-outlook/home-outlook.js", "./core/navigation-bar/navigation-bar.js",
+      "./articles/articles.js", "./intro/intro.js", "./showcase/showcase.js", "./article-detail/article-detail.js"
+    ],
+    output: {
+      filename: "bundle.js"
+    }
   }
-};
+];
