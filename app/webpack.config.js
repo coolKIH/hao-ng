7799b0c04abdfd8eab4a0752b91806c8d16d1456
coolKIH <caozihuahh@gmail.com>
2017-02-16 12:39:22 +0800

    bundled custom js files

---
+++ app/webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hao on 17-2-16.
+ */
+module.exports = {
+  entry: ["./app.js", "./home-outlook/home-outlook.js", "./core/navigation-bar/navigation-bar.js",
+    "./articles/articles.js", "./intro/intro.js", "./showcase/showcase.js", "./article-detail/article-detail.js"
+  ],
+  output: {
+    filename: "bundle.js"
+  }
+};
